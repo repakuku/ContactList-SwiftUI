f10265f2022cf670b0c9de2323650b23734a988d
Alexey Turulin <repakuku@icloud.com>
2023-08-01 18:24:12 -0400

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Alexey Turulin on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
